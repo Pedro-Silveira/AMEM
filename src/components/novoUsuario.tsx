38bee46e0e8ac144a12a43e0513d0c8e2408849b
Pedro Silveira <pedro.silveiradev@gmail.com>
2024-05-06 16:46:22 -0300

    Implementação do botão limpar.

--- src/components/novoUsuario.tsx
+++ src/components/novoUsuario.tsx
@@ -4,6 +4,7 @@ import { auth, db } from "../services/firebaseConfig";
 import { ref, set } from "firebase/database";
 import { useNavigation } from "@react-navigation/native";
 import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import showToast from "../utils/showToast";
 
 const NovoUsuario = () => {
     const [nome, setNome] = useState('');
@@ -12,9 +13,18 @@ const NovoUsuario = () => {
     const [senhaRepeticao, setSenhaRepeticao] = useState('');
     const [permissao, setPermissao] = useState('');
     const [erros, setErros] = useState({});
-    const toast = useToast();
     const navigation = useNavigation();
+    const toast = useToast();
     const logina = auth;
+
+    const limpar = () => {
+        setNome('');
+        setEmail('');
+        setSenha('');
+        setSenhaRepeticao('');
+        setPermissao('');
+        setErros({});
+    };
 
     const validarUsuario = () => {
         const nomeRegex = new RegExp(/[A-Za-z][A-Za-z0-9 ()-]?/);
@@ -86,34 +96,18 @@ const NovoUsuario = () => {
                         email: email,
                         permissao: permissao
                     }).then(() => {
-                        toast.show({
-                            render: () => {
-                                return <Box padding={2} mb={3} rounded="sm" _text={{color: "#fff"}} bg="green.500">O usuário foi cadastrado com sucesso!</Box>;
-                            }
-                        });
+                        showToast(toast, "green.500", "O usuário foi cadastrado com sucesso!");
                         navigation.navigate("Painel de Controle - AMEM" as never);
                     }).catch((error) => {
-                        toast.show({
-                            render: () => {
-                                return <Box padding={2} mb={3} rounded="sm" _text={{color: "#fff"}} bg="red.500">Erro: {error}.</Box>;
-                            }
-                        });
+                        showToast(toast, "red.500", "Erro: " + error);
                     });
                 }).catch((error) => {
-                    toast.show({
-                        render: () => {
-                            return <Box padding={2} mb={3} rounded="sm" _text={{color: "#fff"}} bg="red.500">Erro: {error}.</Box>;
-                        }
-                    });
+                    showToast(toast, "red.500", "Erro: " + error);
                 });
             }
         })
         .catch((error) => {
-            toast.show({
-                render: () => {
-                    return <Box padding={2} mb={3} rounded="sm" _text={{color: "#fff"}} bg="red.500">Erro: {error}.</Box>;
-                }
-            });
+            showToast(toast, "red.500", "Erro: " + error);
         });
     };
 
@@ -128,35 +122,35 @@ const NovoUsuario = () => {
                 <Box marginBottom={25}>
                     <FormControl isRequired isInvalid={'nome' in erros}>
                         <FormControl.Label>Nome:</FormControl.Label>
-                        <Input placeholder="Escolha um nome..." onChangeText={novoNome => setNome(novoNome)}/>
+                        <Input value={nome} placeholder="Escolha um nome..." onChangeText={novoNome => setNome(novoNome)}/>
                         {'nome' in erros ?
                             <FormControl.ErrorMessage leftIcon={<WarningOutlineIcon size="xs" />}>{erros.nome}</FormControl.ErrorMessage>: ''
                         }
                     </FormControl>
                     <FormControl isRequired isInvalid={'email' in erros}>
                         <FormControl.Label>E-mail:</FormControl.Label>
-                        <Input placeholder="Insira o e-mail..." onChangeText={novoEmail => setEmail(novoEmail)}/>
+                        <Input value={email} placeholder="Insira o e-mail..." onChangeText={novoEmail => setEmail(novoEmail)}/>
                         {'email' in erros ?
                             <FormControl.ErrorMessage leftIcon={<WarningOutlineIcon size="xs" />}>{erros.email}</FormControl.ErrorMessage>: ''
                         }
                     </FormControl>
                     <FormControl isRequired isInvalid={'senha' in erros}>
                         <FormControl.Label>Senha:</FormControl.Label>
-                        <Input type="password" placeholder="Escolha uma senha..." onChangeText={novaSenha => setSenha(novaSenha)}/>
+                        <Input value={senha} type="password" placeholder="Escolha uma senha..." onChangeText={novaSenha => setSenha(novaSenha)}/>
                         {'senha' in erros ?
                             <FormControl.ErrorMessage leftIcon={<WarningOutlineIcon size="xs" />}>{erros.senha}</FormControl.ErrorMessage>: ''
                         }
                     </FormControl>
                     <FormControl isRequired isInvalid={'senhaRepeticao' in erros}>
                         <FormControl.Label>Confirmar Senha:</FormControl.Label>
-                        <Input type="password" placeholder="Repita a senha..." onChangeText={novaSenhaRepeticao => setSenhaRepeticao(novaSenhaRepeticao)}/>
+                        <Input value={senhaRepeticao} type="password" placeholder="Repita a senha..." onChangeText={novaSenhaRepeticao => setSenhaRepeticao(novaSenhaRepeticao)}/>
                         {'senhaRepeticao' in erros ?
                             <FormControl.ErrorMessage leftIcon={<WarningOutlineIcon size="xs" />}>{erros.senhaRepeticao}</FormControl.ErrorMessage>: ''
                         }
                     </FormControl>
                     <FormControl isRequired isInvalid={'permissao' in erros}>
                         <FormControl.Label>Permissão:</FormControl.Label>
-                        <Select onValueChange={novaPermissao => setPermissao(novaPermissao)} placeholder="Escolha uma permissão..." _selectedItem={{bg: "teal.600", endIcon: <CheckIcon size={1} />}}>
+                        <Select selectedValue={permissao} onValueChange={novaPermissao => setPermissao(novaPermissao)} placeholder="Escolha uma permissão..." _selectedItem={{bg: "teal.600", endIcon: <CheckIcon size={1} />}}>
                             <Select.Item label="Usuário" value="usuario" />
                             <Select.Item label="Editor" value="editor" />
                             <Select.Item label="Administrador" value="administrator" />
@@ -168,7 +162,7 @@ const NovoUsuario = () => {
                 </Box>
                 <Box flexDirection={"row"}>
                     <Button size={"lg"} backgroundColor={"#1C3D8C"} _hover={{backgroundColor: "#043878"}} flex={1} marginRight={1} onPress={validarUsuario}>Cadastrar</Button>
-                    <Button size={"lg"} backgroundColor={"#bebebe"} _hover={{backgroundColor: "#A6A6A6"}} flex={1} marginRight={1} onPress={() => console.log("hello world")}>Limpar</Button>
+                    <Button size={"lg"} backgroundColor={"#bebebe"} _hover={{backgroundColor: "#A6A6A6"}} flex={1} marginRight={1} onPress={limpar}>Limpar</Button>
                 </Box>
             </Box>
         </ScrollView>
---
+++ src/utils/showToast.tsx
@@ -0,0 +1,9 @@
+import { Box } from "native-base";
+
+export default function showToast(toast: any, bgColor: any, msg: any){
+    toast.show({
+        render: () => {
+            return <Box padding={2} mb={3} rounded="sm" _text={{color: "#fff"}} bg={bgColor}>{msg}</Box>;
+        }
+    });
+}
